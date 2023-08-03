ed0c0019e5e144e9cb5e2e03630f5d729733d4a9
Pedro Marcos <pedro_162_marcos@hotmail.com>
2023-08-03 15:34:49 -0300

    Criado metodo MenuHelpers para separar opçao dos menus

---
+++ src/MenuHelpers.java
@@ -0,0 +1,17 @@
+public class MenuHelpers {
+    public static void exibirMenuPrincipal() {
+        System.out.println();
+        System.out.println("##### BEM VINDO AO RESTAURANTE! #####");
+        System.out.println("Escolha uma opção:");
+        System.out.println("1 - Iniciar uma nova comanda");
+        System.out.println("2 - Encerrar Programa");
+        System.out.println();
+    }
+
+    public static void exibirMenuComanda() {
+        System.out.println("Escolha uma opção:");
+        System.out.println("1 - Adicionar produto à comanda");
+        System.out.println("2 - Exibir produtos disponíveis");
+        System.out.println("0 - Finalizar a comanda");
+    }
+}
